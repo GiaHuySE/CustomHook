962214bc6559859c23ecd0e1cc0afe8112a36d14
GiaHuySE <tonghuy214@gmail.com>
2023-09-13 11:24:22 +0700

    Finish exercise 1,2

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
+import useCountDown from './custom-hook/useCountDown';
+import useTheme from './custom-hook/useTheme'
+
 function App() {
+  const countDown = useCountDown(10, false);
+  const { isDarkMode, colorScheme, toggle, onDarkModeChange } = useTheme(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ padding: 20, backgroundColor: colorScheme.background, height: "100vh" }}>
+      <h1 style={{ color: colorScheme.onBackground }}>{countDown.value}</h1>
+      <p style={{ color: colorScheme.onBackground }}>Countdown Running: {countDown.isRunning ? 'Yes' : 'No'}</p>
+      <button onClick={() => { countDown.start() }} disabled={countDown.isRunning}>Start</button>
+      <button onClick={() => { countDown.stop() }} disabled={!countDown.isRunning}>Stop</button>
+
+      <h1 style={{ color: colorScheme.onBackground }}>change theme</h1>
+
+      <button onClick={toggle} >Turn on {isDarkMode ? "light" : "dark"} mode</button>
     </div>
   );
 }
---
+++ src/custom-hook/useTheme.ts
@@ -0,0 +1,85 @@
+import { useState, useEffect } from "react";
+
+interface ColorScheme {
+    primary: string;
+    secondary: string;
+    background: string;
+    onBackground: string;
+    error: string;
+    success: string;
+    warning: string;
+    disabled: string;
+}
+
+
+interface Theme {
+    isDarkMode: boolean,
+    colorScheme: ColorScheme,
+    toggle: () => void;
+    // onDarkModeChange: (isDarkMode: boolean) => boolean
+    onDarkModeChange: (isDarkMode: boolean) => void
+}
+
+const lightColorScheme: ColorScheme = {
+    primary: "#0b5ed7",
+    secondary: "#5c636a",
+    background: "#FFFFFF",
+    onBackground: "#000000",
+    error: "#B00020",
+    success: "#157347",
+    warning: "#ffca2c",
+    disabled: "#5c636a",
+}
+
+const darkColorScheme: ColorScheme = {
+    primary: "#0b5ed7",
+    secondary: "#5c636a",
+    background: "#000000",
+    onBackground: "#FFFFFF",
+    error: "#B00020",
+    success: "#157347",
+    warning: "#ffca2c",
+    disabled: "#5c636a",
+}
+
+const useTheme = (initialDarkMode: boolean): Theme => {
+    const [isDarkMode, setIsDarkMode] = useState<boolean>(initialDarkMode);
+    const [colorScheme, setColorScheme] = useState<ColorScheme>(isDarkMode ? darkColorScheme : lightColorScheme);
+
+    useEffect(() => {
+        setColorScheme(isDarkMode ? darkColorScheme : lightColorScheme);
+        onDarkModeChange(isDarkMode);
+    }, [isDarkMode]);
+
+    const toggle = () => {
+        if (isDarkMode) {
+            setIsDarkMode(false);
+        } else {
+            setIsDarkMode(true)
+        }
+    }
+
+    // const onDarkModeChange = (isDarkMode: boolean): boolean => {
+    //     if (isDarkMode) {
+    //         return true;
+    //     } else {
+    //         return false;
+    //     }
+    // };
+
+    const onDarkModeChange = (isDarkMode: boolean): void => {
+        if (isDarkMode) {
+            console.log('Dark mode')
+        } else {
+            console.log('Light mode')
+        }
+    }
+    return {
+        isDarkMode,
+        colorScheme,
+        toggle,
+        onDarkModeChange,
+    };
+}
+
+export default useTheme
